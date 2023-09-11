821b7226252c2dbb7cdde3d5e70f10c12448809b
LuAlcantara <alcantara.lucas@terra.com.br>
2023-09-11 12:22:39 -0300

    exerciciojava

---
+++ 1TDSPB/TopProject/src/br/com/fiap/beans/Endereco.java
@@ -0,0 +1,60 @@
+package br.com.fiap.beans;
+
+public class Endereco {
+	
+	private String logradouro;
+	private String cep;
+	private int numero;
+	private String bairro;
+	private String complemento;
+	private String municipio;
+	private String estado;
+	
+	//Getters, Setters
+	
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	
+
+}
